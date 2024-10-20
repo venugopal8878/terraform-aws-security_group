5f9e916c4999c1ac931d3603d38f4e1df9465a18
venugopal <mallavallivenu@gmail.com>
2024-10-20 11:23:36 +0530

    security_group

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "main" {
+  name        =local.sg_final_name
+   #name should be like this expense-dev-mysql
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc_id #user needs to supply
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  tags = merge(
+    var.common_tags,
+    var.sg_tags,
+    {
+    Name = local.sg_final_name
+      }
+  )
+}
