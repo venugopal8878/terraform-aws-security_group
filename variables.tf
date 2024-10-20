1908894ef0f7074e5b67add6be03c618c204ea3f
venugopal8878 <mallavallivenu@gmail.com>
2024-10-20 11:36:37 +0530

    security group

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "project_name"{
+
+}
+
+variable "environment"{
+
+}
+variable "sg_name"{
+
+}
+
+variable "common_tags"{
+    default={}
+}
+variable "sg_tags"{
+    default={}
+}
+
+variable "vpc_id"{
+    
+}
